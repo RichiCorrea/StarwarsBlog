79d12b508ea925e4e449cf0e564c03fed41cee40
RichiCorrea <81774840+RichiCorrea@users.noreply.github.com>
2021-07-07 21:39:13 -0400

    avanzando

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,12 @@ const Footer = () => {
         <footer>
             <div className="row bg-dark">
                 <div className="col-12 d-flex justify-content-center">
-                    <span className="text-white">You can follow me on <a className="text-white" style={{'text-decoration': 'none'}} href="https://github.com/zero-skill">GitHub!</a> </span>
+                    <span className="text-white">Thanks to <a className="text-white" style={{'text-decoration': 'none'}} href="https://www.swapi.tech/">SWAPI</a> </span>
                 </div>
             </div>
             <div className="row pt-1 bg-dark ">
                 <div className="col-12 d-flex justify-content-center">
-                    <span className="text-white ">Made with ♥ by </span>
-                    <span className="text-white ms-1"><strong> zero-skill</strong></span>
+                    <span className="text-white ">for sharing this information </span>
                 </div>
                 
             </div>
